45083bf015d128978f45e94e5a3cc73e4fa6f0e9
doctore2com <milanoitaliana@gmail.com>
2024-10-12 08:08:38 +0200

    initial commit after a huge brake

---
+++ app/server/src/main/java/com/example/repository/BoatRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.boat.Boat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoatRepository extends JpaRepository<Boat,Long>{
+
+}
