051bccab8c24253998bdd7a5ed1a016cb7cf7845
doctore2com <milanoitaliana@gmail.com>
2024-10-22 19:26:53 +0200

    docker setup

--- app/server/src/main/java/com/example/security/WebSecurityConfig.java
+++ app/server/src/main/java/com/example/security/WebSecurityConfig.java
@@ -160,6 +160,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+				.antMatchers("/boats/**").permitAll()
                 .anyRequest().authenticated();
 
         http.authenticationProvider(authenticationProvider());
