933aea63f6b34b2b20b20c4b952e65bedafcffdc
doctore2com <38701595+doctore2com@users.noreply.github.com>
2024-11-30 08:29:49 +0100

    added relation to charter class

--- app/server/src/main/java/com/example/charter/Charter.java
+++ app/server/src/main/java/com/example/charter/Charter.java
@@ -33,8 +33,8 @@ public class Charter {
     @Column(name="end_charter")
     private Date endCharter;
     private String port;
-    @ManyToOne
-    @JoinColumn(name="id")
+    @ManyToOne(fetch =FetchType.LAZY)
+    @JoinColumn(name="user_id")
     private User user;
 
 
